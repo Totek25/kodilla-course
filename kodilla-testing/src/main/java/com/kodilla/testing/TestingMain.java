3161465d9944002ba67acbf8642de61fbfbf0c2a
Tomek Toczynski <tomek-toczynski@wp.pl>
2022-09-17 13:19:07 +0200

    6.2 Calculator Test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        System.out.println("Test dodawania");
+
+        int result = calculator.add(2,4);
+
+        if (result == 6) {
+            System.out.println("Wynik jest OK");
+        } else {
+            System.out.println("Wynik nie jest OK");
+        }
+
+        System.out.println("Test odejmowania");
+
+        result = calculator.sub(2,4);
+
+        if (result == -2) {
+            System.out.println("Wynik jest OK");
+        } else {
+            System.out.println("Wynik nie jest OK");
+        }
     }
 }
